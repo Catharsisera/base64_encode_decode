ebb718747ad2abd83e7da49ea6d938477c8d5b90
GeroinovayaLady <k.mitushkina00@mail.ru>
2021-11-24 02:31:51 +0300

    import base64

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+import base64
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+message = "Today is a great day"
+message_bytes = message.encode('ascii')
+base64_bytes = base64.b64encode(message_bytes)
+base64_message = base64_bytes.decode('ascii')
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(base64_message)
